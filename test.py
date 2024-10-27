991a9fba2738530b24dbada29fa852c07e2db106
adinkojic <kojic@princeton.edu>
2024-10-26 22:43:28 -0400

    come on

--- test.py
+++ test.py
@@ -4,9 +4,5 @@ msg = "Roll a dice"
 print(msg)
 
 print(np.random.randint(1,9))
-<<<<<<< HEAD
-print("hi akash just made an edit")
-=======
 
 print("enter a message")
->>>>>>> 0aa1e01f496d2d6e363526b89fdf7a9a6a342c14
