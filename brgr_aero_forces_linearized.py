caf3ce261890776fdf569b944b45e6c767dde1de
adinkojic <kojic@princeton.edu>
2024-12-04 23:39:47 -0500

    idk some aero stuff

--- brgr_aero_forces_linearized.py
+++ brgr_aero_forces_linearized.py
@@ -9,9 +9,9 @@ TODO: simulation for high altitude'''
 
 #gives in body frame
 def from_alpha_beta(airspeed, alpha, beta):
-    x = airspeed * math.cos(alpha * math.pi()/180) * math.cos(beta * math.pi()/180)
-    y = airspeed * math.sin(beta  * math.pi()/180)
-    z = airspeed * math.sin(alpha * math.pi()/180) * math.cos(beta * math.pi()/180)
+    x = airspeed * math.cos(alpha * math.pi/180) * math.cos(beta * math.pi/180)
+    y = airspeed * math.sin(beta  * math.pi/180)
+    z = airspeed * math.sin(-alpha * math.pi/180) * math.cos(beta * math.pi/180)
 
     return np.array([x,y,z])
 
@@ -20,9 +20,9 @@ def velocity_to_alpha_beta(velocity_body):
     airspeed = math.sqrt(velocity_body[0]**2 + velocity_body[1]**2 + velocity_body[2]**2)
     temp = math.sqrt(velocity_body[0]**2 + velocity_body[2]**2)
     beta = math.atan2(velocity_body[1], temp)
-    alpha = math.atan2(velocity_body[2], velocity_body[0])
+    alpha = -math.atan2(velocity_body[2], velocity_body[0])
 
-    return airspeed, beta, alpha
+    return airspeed, alpha, beta
 
 #TODO: implement atmosphere model
 @jit
@@ -40,7 +40,8 @@ def get_Re(airspeed, density, viscosity):
 #TODO: implement based on better data and with Re
 @jit
 def get_coeff(alpha, beta, Re, p, q ,r):
-    alpha = np.clip(alpha, -15*math.pi/180, 15*math.pi/180)
+    aa = np.array([alpha]).clip(-15*math.pi/180, 15*math.pi/180)
+    alpha = aa[0]
 
     C_L0 = 0.34
     C_La = 4.
@@ -51,7 +52,7 @@ def get_coeff(alpha, beta, Re, p, q ,r):
     C_Mq = -0.01 #not sure about this value
 
     C_L = C_L0 + C_La * alpha
-    C_D = C_D0 + epsilon * C_L^2
+    C_D = C_D0 + epsilon * C_L**2
     C_M = C_M0 + C_Ma * alpha #+ C_Mq * q
 
     C_Y = 0 #yaw
@@ -73,7 +74,7 @@ def get_aero_forces(state):
     Aref = 0.2 #m**2
     mac = 0.1778 #m
 
-    airspeed, beta, alpha = velocity_to_alpha_beta(np.array(state[3],state[4],state[5]))
+    airspeed, alpha, beta = velocity_to_alpha_beta(np.array([state[3],state[4],state[5]]))
 
     altitude = state[2]
 
@@ -82,23 +83,23 @@ def get_aero_forces(state):
 
     p, q, r = state[10], state[11], state[12]
 
-    C_L,C_D,C_M, C_Z, C_Y, C_R = get_coeff(alpha, beta, Re, p, q ,r)
+    C_L, C_D, C_M, C_Z, C_Y, C_R = get_coeff(alpha, beta, Re, p, q ,r)
 
     qbar = 0.5 * density *airspeed**2
 
-    lift = C_L * qbar * Aref
-    drag = C_L * qbar * Aref
-    side = C_Z * qbar * Aref
-    pitching_moment = C_M * qbar * Aref * mac
-    yawing_moment   = C_Y * qbar * Aref #Coeff definition might be wrong
-    rolling_moment  = C_R * qbar * Aref
+    body_lift = C_L * qbar * Aref
+    body_drag = C_D * qbar * Aref
+    body_side = C_Z * qbar * Aref
+    body_pitching_moment = C_M * qbar * Aref * mac
+    body_yawing_moment   = C_Y * qbar * Aref #Coeff definition might be wrong
+    body_rolling_moment  = C_R * qbar * Aref
 
     wind_to_stab = get_wind_to_stability_axis(alpha,beta)
 
-    body_forces_wind = np.array([-drag, side, lift])
+    body_forces_wind = np.array([-body_drag, body_side, body_lift])
     body_forces_stab = quat.rotateVectorQ(wind_to_stab, body_forces_wind)
 
-    moments = np.array([rolling_moment, -pitching_moment, yawing_moment])
+    moments = np.array([body_rolling_moment, -body_pitching_moment, body_yawing_moment])
 
     return body_forces_stab, moments
     
--- test.py
+++ test.py
@@ -3,20 +3,14 @@ from numba import jit
 import math
 import quaternion_math as quat
 import wgs84
+import brgr_aero_forces_linearized as aero
 
+@jit
+def yer(aa):
+    bruh = np.array([aa]).clip(-5,5)
+    return bruh[0]
+velocity = aero.from_alpha_beta(15, 5, 0)
 
-nose = np.array([1, 0, 0])
-left = np.array([0, 1, 0])
-tail = np.array([0, 0, 1])
+test_state = np.array([0, 0, 0, velocity[0], velocity[1] , velocity[2], 0, 0 ,0, 0, 0, 0, 0])
 
-roll = 90
-pitch = 90
-yaw = 90
-
-
-ori = quat.from_euler(roll * math.pi/180, pitch * math.pi/180, yaw * math.pi/180)
-
-print(ori)
-print(quat.rotateFrameQ(ori, nose))
-print(quat.rotateFrameQ(ori, left))
-print(quat.rotateFrameQ(ori, tail))
+print(aero.get_aero_forces(test_state))
